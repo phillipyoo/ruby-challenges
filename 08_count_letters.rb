ae22faa7db79518533e709c765b7eafeea0627ca
Dong Yoo <phillipyoo@Dongs-MacBook-Air.local>
2019-11-08 14:46:10 +1000

    completed challenges

--- 08_count_letters.rb
+++ 08_count_letters.rb
@@ -10,8 +10,26 @@
 # Check your solution by running the tests:
 # ruby tests/08_count_letters_test.rb
 
-def count_letters (string)
-  result = {} # You'll need an empty hash to get started!
+# h = Hash.new(0)
+#   #=> {}
+# We now do the following:
 
-  return result # return the hash
+# arr.each { |c| h[c] += 1 }
+
+
+
+def count_letters(string)
+  result = {} # You'll need an empty hash to get started!
+  count = Hash.new(0)
+  string.each_char do |letter|
+    if result[letter]
+      result[letter] += 1
+    else
+      result[letter] = 1
+    end 
+  end  
+  result.sort_by {|letter, count| count}
+p result
 end
+
+count_letters("hello")
